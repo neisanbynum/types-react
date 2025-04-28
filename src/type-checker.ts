62fec33dc192abbbf2f4bcb5d10f3162101b12ec
Nathan B <emmanuel.n.bynum2024@outlook.com>
2025-04-28 13:21:15 -0500

    Fixed the isFC type checker

--- src/type-checker.ts
+++ src/type-checker.ts
@@ -1,3 +1,9 @@
 export function isFC(component: Function): component is React.FC {
-	return component.prototype && component.prototype.isReactComponent
+	return (
+		typeof component === 'function' &&
+		!(
+			component.prototype &&
+			component.prototype.isReactComponent
+		)
+	)
 }
